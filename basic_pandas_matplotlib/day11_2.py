7ebfc028fcc65cbe4a3330c09d1394176a636f04
tsubasaman <luvdry@gmail.com>
2025-11-06 18:13:08 +0900

    update_bar_graph_next_makedir_ba_x_adjut

--- basic_pandas_matplotlib/day11_2.py
+++ basic_pandas_matplotlib/day11_2.py
@@ -9,7 +9,8 @@ import os
 today       = dt.date.today().strftime("%Y_%m_%d")
 folder      = "A_monthly_reports"
 file_path   = "data_files"
-file_list   =  os.listdir(file_path)
+file_list   = os.listdir(file_path)
+os.makedirs(folder, exist_ok=True)
 
 data        = []
 
@@ -17,25 +18,23 @@ for file_name in file_list:
     file = pd.read_csv(f"{file_path}/{file_name}")
     data.append(file)
 
-os.makedirs(folder, exist_ok=True)
-
-
 
 # 2:トータル売り上げと平均価格、棒グラフ用データなどのデータ作成
 
 tabel_out_data = []
 graph_out_data = []
 
-for f in data:
-    f["total"]    	 	  = f["price"] * f["count"]
+for i, f in enumerate(data):
+    fig,axes    = plt.subplots(1, 2, figsize=(10, 5))
+    data_label              = file_list[i].replace("data_", "").replace(".csv", "")
+    os.makedirs(f"{folder}/report_{data_label}", exist_ok=True)
+    f["total"]    	 	    = f["price"] * f["count"]
     total_sales			    = round(f["total"].sum())
     average_price   		= round(f["price"].mean())
-    total_sales_rank		= f.sort_values("total").reset_index()
-    top_sales_item			= f["item"][f["total"].idxmax()]
-    top_sales_item_total= f["total"].max()
-    print(top_sales_item_total)
-    top_share_item_rate = round(top_sales_item_total / total_sales * 100, 1)
-    summary_row     	= pd.DataFrame([{
+    top_sales_item_name		= f["item"][f["total"].idxmax()]
+    top_sales_item_total    = f["total"].max()
+    top_share_item_rate     = round(top_sales_item_total / total_sales * 100, 1)
+    summary_row     	    = pd.DataFrame([{
         "item" : "total_sales",
         "count" : None,
         "price" : None,
@@ -48,20 +47,33 @@ for f in data:
     ])
     
     tabel_out_data.append(pd.concat([f, summary_row]))
-    graph_out_data.append(total_sales_rank)
+    graph_out_data = f.sort_values("total").reset_index()
 
-print(graph_out_data)
-
-# 3: AIコメント
-
-ai_comment = f"総売上は {total_sales} 円、平均単価は {average_price} 円です。最も売れた商品は「{top_sales_item}」で、全体の {top_share_item_rate}% を占めています。全体的に、上位商品の売上が構成比の大部分を占める傾向があります"
-
-# 4:csvファイルごとにデータ作成
-
-fig,axes = plt.subplots(1, 2, tight_layout=True)
-
-for data in tabel_out_data:
-	
-	axes[0].bar(
-		data["item"],
-		data["total"])
+    ai_comment = (
+        f" {data_label} 月の総売上は {total_sales} 円、平均単価は {average_price} 円です。\n"
+        f"最も売れた商品は「{top_sales_item_name}」で、全体の {top_share_item_rate}% を占めています。\n"
+        f"全体的に、上位商品の売上が構成比の大部分を占める傾向があります"
+        )
+    
+    # 棒グラフの作成
+    bars = axes[0].bar(
+        graph_out_data["item"],
+        graph_out_data["total"],
+        color="blue"
+    )
+    axes[0].set_xlabel("item")
+    axes[0].set_ylabel("total_sales(JPY)")
+    for bar in bars:
+        x = bar.get_x() + bar.get_width()/2
+        y = bar.get_height()
+        axes[0].text(
+            x,
+            y,
+            int(y),
+            ha="center",
+            va="bottom"
+            
+        )
+    print(graph_out_data)
+    plt.savefig(f"{folder}/{data_label}.png")
+    plt.close(fig)

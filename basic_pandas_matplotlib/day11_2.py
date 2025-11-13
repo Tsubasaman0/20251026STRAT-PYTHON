2e81bca6613885daf959851f945fb968219049ba
Tsubasaman <57704006+Tsubasaman0@users.noreply.github.com>
2025-11-13 12:50:54 +0900

    コミットリセット後、トータルセールスとアベレージプライスについてのコメント追加のみ

--- basic_pandas_matplotlib/day11_2.py
+++ basic_pandas_matplotlib/day11_2.py
@@ -40,6 +40,7 @@ for csv_path in csv_files:
     df["count"] = pd.to_numeric(df["count"], errors="coerce").fillna(0).astype(int)
     df["total"]           	= df["price"] * df["count"]
      
+    # 合計売上と単価の加重平均金額
     total_sales			    = int(df["total"].sum())
     total_count             = int(round(df["count"].sum()))
     avg_price_weighted   		= int(round(total_sales / total_count if total_count > 0 else 0))

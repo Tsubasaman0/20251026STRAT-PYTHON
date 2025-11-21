776fada659164651c9b06cefd5013015edebcff5
tsubasaman <luvdry@gmail.com>
2025-11-21 22:39:30 +0900

    v0.1 練習用フォルダと本番用ファイルの作成

--- basic_pandas_matplotlib/ads_line.py
+++ basic_pandas_matplotlib/ads_line.py
@@ -3,29 +3,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# ads.csv を読み込む（自分のパスに合わせる）
-df_ads = pd.read_csv("ads.csv")
+# ads.csv を読み込む
+df_ads =pd.read_csv("ads.csv")
 
-# X, y
+# x, y
 X = df_ads[["ad_cost"]]
 y = df_ads["sales"]
 
-# モデル
+# モデル学習
 model = LinearRegression()
 model.fit(X, y)
 
-# coef, intercept
+
+# a, b を取得
 a = model.coef_[0]
 b = model.intercept_
 
-# 直線描画用
+print("a: (coef_):", a)
+print("b: (intercept_):", b)
+
+# 直線の点を作る
 x_line = np.linspace(df_ads["ad_cost"].min(), df_ads["ad_cost"].max(), 100)
 y_line = a * x_line + b
 
 # グラフ
-plt.scatter(df_ads["ad_cost"], df_ads["sales"], color="blue")
-plt.plot(x_line, y_line, color="red")
-plt.title("Linear Regression (ads)")
+plt.scatter(df_ads["ad_cost"], df_ads["sales"], label="data")
+plt.plot(x_line, y_line, label="y = ax + b")
+plt.legend()
+plt.title("Regression line and intercept")
 plt.xlabel("ad_cost")
 plt.ylabel("sales")
+plt.savefig("intercept.png")
 plt.show()
